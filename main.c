00bba7a42dff1cd780e03a73f855c4c7ad9f3a3c
Christoph <christoph.geitner@gmail.com>
2017-06-14 08:54:27 +0200

    Erster Upload

---
+++ main.c
@@ -0,0 +1,36 @@
+/******************************************************************************
+ *
+ * Project: Hindernisumfahrung mit dem RP6
+ * File:    main.c
+ *
+ * Ingenieurorientierte Programmierung
+ * Dr. Alexander Kling
+ *
+ * Carsten Bevermann
+ * Christoph Geitner
+ * Jens Reimer
+ * Marc Töpker
+ *
+ *****************************************************************************/
+
+//#include <avr/io.h>
+#include <RP6RobotBaseLib.h>
+
+int main(void)
+{
+    initRobotBase();
+
+    mSleep(1500);
+
+    powerON(); // Encoder und Motorstromsensoren anschalten!
+
+    while(1)
+    {
+        //
+    }
+
+    return 0;
+}
+
+
+/******************** End of file <main.c> ***********************************/
