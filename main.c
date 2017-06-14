a02605b4124776e335d3f39148e9bb35b843869d
MarcToepker <marc.toepker@hs-osnabrueck.de>
2017-06-14 11:17:22 +0200

    11:17

--- main.c
+++ main.c
@@ -14,103 +14,81 @@
  *****************************************************************************/
 
 //#include <avr/io.h>
-#include <RP6RobotBaseLib.h>
-enum Zustand
+#include "RP6RobotBaseLib.h"
+
+int destination = 1000;
+
+enum state_model
 {
     IDLE,
-    TURN_LEFT,
-    TURN_RIGHT,
-    WAITING
-};
-struct RP6_State
+    BUMPER_LEFT,
+    BUMPER_RIGHT,
+    MOVE_TO_DESTINATION,
+} state;
+
+
+void bumperActive(void)
 {
-    enum Zustand state;
-}rp6_state;
-struct RP6_settings
-{
-    uint8_t rotate_speed;
-    uint16_t angle;
-    uint8_t dir;
-    uint8_t block;
-    uint8_t complete;
-    uint8_t warning;
+    if (bumper_left)
+    {
+        state = BUMPER_LEFT;
+    }
+    if (bumper_right)
+    {
+        state = BUMPER_RIGHT;
+    }
 }
-rp6_set;
 
-uint8_t move_state = rp6_state.IDLE;
-
-void move_stateMachine(void)
+void stateModel(void)
 {
-	switch( )
+	switch(state)
 	{
-		case STATE_START:
-			setLEDs(0b010000);
-			move_state = STATE_WAIT_FOR_BUMPER_HIT_LEFT;
+		case IDLE:
+		    stop();
 		break;
 
-		case STATE_WAIT_FOR_BUMPER_HIT_LEFT:
-			if(getStopwatch1() > 500)
+		case MOVE_TO_DESTINATION:
+            move(200, FWD, DIST_MM(destination),true);
+
+            if(isMovementComplete())
 			{
-				statusLEDs.LED5 = !statusLEDs.LED5;
-				updateStatusLEDs();
-				setStopwatch1(0);
+                state = IDLE;
 			}
-			if(bumper_left)
-			{
-				setLEDs(0b011001);
-				move(50, BWD, DIST_MM(150), NON_BLOCKING);
-				move_state = STATE_MOVE_BACKWARDS;
-			}
-		break;
-		case STATE_MOVE_BACKWARDS:
-			if(isMovementComplete())
-			{
-				setLEDs(0b000010);
-				move_state = STATE_WAIT_FOR_BUMPER_HIT_RIGHT;
-			}
+        break;
+
+		case BUMPER_LEFT:
+            move(150, BWD, DIST_CM(10), true);
+            rotate(150, RIGHT, 90, true); // um 180° nach links drehen
+            move(150, FWD, DIST_CM(10), true);
+            rotate(150, LEFT, 90, true); // um 180° nach links drehen
+            state = MOVE_TO_DESTINATION;
 		break;
 
-		case STATE_WAIT_FOR_BUMPER_HIT_RIGHT:
-			if(getStopwatch1() > 500)
-			{
-				statusLEDs.LED2 = !statusLEDs.LED2;
-				updateStatusLEDs();
-				setStopwatch1(0);
-			}
-			if(bumper_right)
-			{
-				setLEDs(0b100110);
-				move(50, FWD, DIST_MM(150), NON_BLOCKING);
-				move_state = STATE_MOVE_FORWARDS;
-			}
+        case BUMPER_RIGHT:
+            move(150, BWD, DIST_CM(10), true);
+            rotate(150, LEFT, 90, true); // um 180° nach links drehen
+            state = MOVE_TO_DESTINATION;
 		break;
-		case STATE_MOVE_FORWARDS:
-			if(isMovementComplete())
-				move_state = STATE_START;
-		break;
+
 	}
 }
-
-
-
 
 int main(void)
 {
 	initRobotBase();
-	setLEDs(0b111111);
 	mSleep(1000);
-
 	powerON();
+	//initial value
+    state = MOVE_TO_DESTINATION;
 
-	startStopwatch1();
 
+	BUMPERS_setStateChangedHandler(bumperActive);
 	while(true)
 	{
-		move_stateMachine();
+		stateModel();
 		task_RP6System();
 	}
 	return 0;
 }
-
 
 /******************** End of file <main.c> ***********************************/
